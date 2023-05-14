ee860e7e22af2991201d6a4b65c06cf7f8507a15
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-05-14 20:35:06 +0600

    Create 01_C_Comments.c

---
+++ 01_C_Comments.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+  
+  printf("\nC Comments!"); // A Comment
+  
+  // This is single line comment
+  
+  /* This is a
+  multi-line
+  comment */
+  
+  return 0;
+}
