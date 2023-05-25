e664488497b14a513e26ff70f311df42e0295443
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-05-25 22:55:48 +0600

    Create 13_C_Functions.c

---
+++ 13_C_Functions.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// Creating a Function
+void myFunction() { printf("Hi, This is a function from the outside!\n"); }
+
+int main(void) {
+
+  // Predefined Functions
+  printf("Hi, I am Zaber!\n");
+
+  // Calling a Function
+  myFunction();
+
+  return 0;
+}
